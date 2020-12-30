a213a1a10f3ae34c5ee10538add387d34f249069
johnny1991524 <johnny1991524@gmail.com>
2020-12-31 07:20:25 +0800

    update webconfig for build

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,16 @@ configureWebpack = {
     }
 }
 
+let targetArgIndex = 0
+process.argv.forEach((val, index) => {
+    if (val === "--target") {
+        targetArgIndex = index + 1
+    }
+});
+
+const isBuildLib = process.argv[targetArgIndex] === "lib"
+
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? "https://jchou24.github.io/Demos/PagePreviewScrollbar/" : "/",
-    configureWebpack: process.env.NODE_ENV === "production" ? configureWebpack : {}
+    configureWebpack: process.env.NODE_ENV === "production" && isBuildLib ? configureWebpack : {}
 }
